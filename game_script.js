c8a7544553bac2e968ea15be7b9c7da4425c268e
Joe Weber <joe@joeweberdesign.com>
2022-10-06 16:25:09 -0700

    add game end on round 5 and win loss count

--- game_script.js
+++ game_script.js
@@ -48,50 +48,127 @@ function selectWinner(computerPlayer, playerOne) {
     return result;
 }
 
-function playGame() {
+// **WHEN PLAYER INPUT TEXT IN CONSOLE LOG
+// function playGame() {
     
-    let playerWins = 0;
+//     let playerWins = 0;
 
-    for (let i = 0; i < 5; i++) {
-        if (playerWins < 3){
-            let playerInput = prompt("Are you a rock, paper or scissors? ")
-            const playerSelection = playerSelectionCheck(playerInput); // btn input
-            const computerSelection = getcomputerSelection();
-            let roundResult = selectWinner(computerSelection, playerSelection);
-            if (roundResult == 'Winner!') {
-                playerWins = playerWins +1;
-            }
-            else if (roundResult == 'Loser!') {
-                playerWins = playerWins -1;
-            }                  
-            console.log(playerSelection);
-            console.log(computerSelection);
-            console.log(roundResult)
-        }
-        else if (playerWins == -3) {
-            return console.log('You lost the game.');
-        }  
-        else {
-            return console.log('You won the game!');
-        } 
+//     for (let i = 0; i < 5; i++) {
+//         if (playerWins < 3){
+//             let playerInput = prompt("Are you a rock, paper or scissors? ")
+//             const playerSelection = playerSelectionCheck(playerInput); // btn input
+//             const computerSelection = getcomputerSelection();
+//             let roundResult = selectWinner(computerSelection, playerSelection);
+//             if (roundResult == 'Winner!') {
+//                 playerWins = playerWins +1;
+//             }
+//             else if (roundResult == 'Loser!') {
+//                 playerWins = playerWins -1;
+//             }                  
+//             console.log(playerSelection);
+//             console.log(computerSelection);
+//             console.log(roundResult)
+//         }
+//         else if (playerWins == -3) {
+//             return console.log('You lost the game.');
+//         }  
+//         else {
+//             return console.log('You won the game!');
+//         } 
 
-    }
-    function gameCount(count) {
-        return (count > 0 ? 'You won! Barely...' : 'You lost! Barely...');
-    }
-    console.log(gameCount(playerWins));
-    console.log(playerWins);
+//     }
+//     function gameCount(count) {
+//         return (count > 0 ? 'You won! Barely...' : 'You lost! Barely...');
+//     }
+//     console.log(gameCount(playerWins));
+//     console.log(playerWins);
     
-}
+// }
 
-function displayText(m) {
+
+
+function playRound(m) {
     const computerSelection = getcomputerSelection();
     let roundResult = selectWinner(computerSelection,m.textContent);
-    console.log(roundResult);
+    
+    gameResult.textContent = roundResult;
+    compChoice.textContent = "AI: "+computerSelection;
+    playChoice.textContent = "You: "+m.textContent;
+    if (roundResult == 'Winner!') {
+        count++;
+        wins++;
+    }
+    else if (roundResult == 'Loser!') {
+        count++;
+        losses++;
+    }
+    else {
+        count++;
+    }
 }
+
 const rpsBtn = document.querySelectorAll('.gameBtn');
+const roundsDisp = document.querySelector('.rounds');
+
+const playChoice = document.querySelector('#play');
+const compChoice = document.querySelector('#comp');
+const gameResult = document.querySelector('#result');
+
+let count = 0;
+let losses = 0;
+let wins = 0;
+
+function winState() {
+    gameResult.textContent = "You won!"
+    compChoice.textContent = "--";
+    playChoice.textContent = "--";
+}
+function loseState() {
+    gameResult.textContent = "You lose!"
+    compChoice.textContent = "--";
+    playChoice.textContent = "--";
+}
+function drawState() {
+    gameResult.textContent = "It's a draw!"
+    compChoice.textContent = "--";
+    playChoice.textContent = "--";
+}
+
+function gameCount() {
+    roundsDisp.innerHTML = count + "/5";
+    if (count === 5) {
+        if (wins > losses) {
+            winState();
+        }
+        else if (wins < losses) {
+            loseState();
+        }
+        else {
+            drawState();
+        }
+        rpsBtn.forEach(function(currentBtn){
+            currentBtn.disabled = true;
+        })
+    }
+}
+
 rpsBtn.forEach(function(currentBtn){
-    currentBtn.addEventListener('click', function(){displayText(currentBtn)});
+    currentBtn.addEventListener('click', 
+        function(){
+        playRound(currentBtn),
+        gameCount()
+        }
+    );
 })
+
+
+
+
+
+
+
+
+
+
 
 
